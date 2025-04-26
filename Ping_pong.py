9091af80300b3c29a982cbafef845dc4525de0db
Anna-N2 <nasytukanna@gmail.com>
2025-04-26 13:32:51 +0300

    Ping_pong.py

--- Ping_pong.py
+++ Ping_pong.py
@@ -38,7 +38,7 @@ class Player(GameSprite):
  back = (200, 255, 255)
  win_width = 600
  win_height = 500
- window = display.set_mode((win_width, win_height)
+ window = display.set_mode((win_width, win_height))
  window.fill(back)
 
 game = True
